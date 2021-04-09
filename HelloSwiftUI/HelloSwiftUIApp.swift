57eed96c1ff6e7bb7d1787558ecb2c936ca695b6
outstandingboy <outstandingboy@kakao.com>
2021-04-09 12:53:33 +0900

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by 송인걸 on 2021/04/09.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
